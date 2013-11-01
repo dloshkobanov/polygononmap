ae5bb61cef8aef53b40a811c188b482b2ee02ef8
master <d.loshkobanov@gmail.com>
2013-11-01 17:43:41 +0400

    modify

--- app/func.py
+++ app/func.py
@@ -9,26 +9,26 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-#РђР»РіРѕСЂРёС‚Рј Р“СЂСЌС…СЌРјР°
+#Алгоритм Грэхэма
 def grahamscan(A):
-    n = len(A) # С‡РёСЃР»Рѕ С‚РѕС‡РµРє
-    P = list(range(n)) # СЃРїРёСЃРѕРє РЅРѕРјРµСЂРѕРІ С‚РѕС‡РµРє
+    n = len(A) # число точек
+    P = list(range(n)) # список номеров точек
     for i in range(1,n):
-      if A[P[i]][0]<A[P[0]][0]: # РµСЃР»Рё P[i]-Р°СЏ С‚РѕС‡РєР° Р»РµР¶РёС‚ Р»РµРІРµРµ P[0]-РѕР№ С‚РѕС‡РєРё
-        P[i], P[0] = P[0], P[i] # РјРµРЅСЏРµРј РјРµСЃС‚Р°РјРё РЅРѕРјРµСЂР° СЌС‚РёС… С‚РѕС‡РµРє
-    for i in range(2,n): # СЃРѕСЂС‚РёСЂРѕРІРєР° РІСЃС‚Р°РІРєРѕР№
+      if A[P[i]][0]<A[P[0]][0]: # если P[i]-ая точка лежит левее P[0]-ой точки
+        P[i], P[0] = P[0], P[i] # меняем местами номера этих точек
+    for i in range(2,n): # сортировка вставкой
       j = i
       while j>1 and (rotate(A[P[0]],A[P[j-1]],A[P[j]])<0):
         P[j], P[j-1] = P[j-1], P[j]
         j -= 1
-    S = [P[0],P[1]] # СЃРѕР·РґР°РµРј СЃС‚РµРє
+    S = [P[0],P[1]] # создаем стек
     for i in range(2,n):
       while rotate(A[S[-2]],A[S[-1]],A[P[i]])<0:
         del S[-1] # pop(S)
       S.append(P[i]) # push(S,P[i])
     return S
 
-#РћРїСЂРµРґРµР»РµРЅРёРµ С†РµРЅС‚СЂР° С‚СЏР¶РµСЃС‚Рё РјРЅРѕРіРѕСѓРіРѕР»СЊРЅРёРєР°
+#Определение центра тяжести многоугольника
 def centeroffravity(A):
     x = 0
     y = 0
@@ -41,12 +41,12 @@ def rotate(A,B,C):
     return (B[0]-A[0])*(C[1]-B[1])-(B[1]-A[1])*(C[0]-B[0])
 
 """
-lstPoint = [(1,1),(1,-1),(0,0),(-1,1),(-1,-1)]# РёСЃС…РѕРґРЅС‹Р№ СЃРїРёСЃРѕРє
-numPolygon = grahamscan(lstPoint)# РЅРѕРјРµСЂР° С‚РѕС‡РµРє РјРЅРѕРіРѕСѓРіРѕР»СЊРЅРёРєР°
-numInternal = set(range(len(lstPoint))) - set(numPolygon)# РЅРѕРјРµСЂР° РІРЅСѓС‚СЂРµРЅРЅРёС… С‚РѕС‡РµРє
-lstPolygon = [lstPoint[i] for i in numPolygon]# СЃРїРёСЃРѕРє С‚РѕС‡РµРє РјРЅРѕРіРѕСѓРіРѕР»СЊРЅРёРєР°
-lstInternal = [lstPoint[i] for i in numInternal]# СЃРїРёСЃРѕРє РІРЅСѓС‚СЂРµРЅРЅРёС… С‚РѕС‡РµРє
-centerPoint= centeroffravity(lstPolygon)# С†РµРЅС‚РµСЂ С‚СЏР¶РµСЃС‚Рё РјРЅРѕРіРѕСѓРіРѕР»СЊРЅРёРєР°
+lstPoint = [(1,1),(1,-1),(0,0),(-1,1),(-1,-1)]# исходный список
+numPolygon = grahamscan(lstPoint)# номера точек многоугольника
+numInternal = set(range(len(lstPoint))) - set(numPolygon)# номера внутренних точек
+lstPolygon = [lstPoint[i] for i in numPolygon]# список точек многоугольника
+lstInternal = [lstPoint[i] for i in numInternal]# список внутренних точек
+centerPoint= centeroffravity(lstPolygon)# центер тяжести многоугольника
 
 
 print(lstPoint)
--- app/views.py
+++ app/views.py
@@ -16,14 +16,14 @@ from app import func
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title = 'Р’С‹Р±РµСЂРёС‚Рµ С‚РѕС‡РєРё РЅР° РєР°СЂС‚Рµ')
+    return render_template('index.html', title = 'Выберите точки на карте')
 
 @app.route('/<hash>')
 def coordinates(hash=None):
-    #РёС‰РµРј РІ Р±Р°Р·Рµ РІСЃРµ РєРѕРѕСЂРґРёРЅР°С‚С‹ РїРѕ С…СЌС€Сѓ
-    #РїСЂРµРґРїРѕР»РѕР¶РёРј РєРѕРѕСЂРґРёРЅР°С‚С‹ Р±С‹Р»Рё С‚Р°РєРёРјРё
+    #ищем в базе все координаты по хэшу
+    #предположим координаты были такими
     lstPoint = [(57.616667,39.85),(57.616667,39.85), (56.616667,39.85), (57.616667,38.85), (56.616667,38.85), (58.616667,40.85), (58.616667,39.85), (57.616667,40.85) ,(60.616667,39.85)]
-    numPolygon = func.grahamscan(lstPoint)# РЅРѕРјРµСЂР° С‚РѕС‡РµРє РјРЅРѕРіРѕСѓРіРѕР»СЊРЅРёРєР°
-    lstPolygon = [lstPoint[i] for i in numPolygon]# СЃРїРёСЃРѕРє С‚РѕС‡РµРє РјРЅРѕРіРѕСѓРіРѕР»СЊРЅРёРєР°
-    centerPoint= func.centeroffravity(lstPolygon)# С†РµРЅС‚РµСЂ С‚СЏР¶РµСЃС‚Рё РјРЅРѕРіРѕСѓРіРѕР»СЊРЅРёРєР°
+    numPolygon = func.grahamscan(lstPoint)# номера точек многоугольника
+    lstPolygon = [lstPoint[i] for i in numPolygon]# список точек многоугольника
+    centerPoint= func.centeroffravity(lstPolygon)# центер тяжести многоугольника
     return render_template('coordinates.html', centerPoint=centerPoint, lstPolygon=lstPolygon, lstPoint=lstPoint)

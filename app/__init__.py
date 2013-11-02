f3f3c99d03dd4b5cfa2f2cedd919925e523c869a
master <d.loshkobanov@gmail.com>
2013-11-02 23:57:20 +0400

    q

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 # coding: utf8
 from flask import Flask
-
 app = Flask(__name__)
 from app import views
--- run.py
+++ run.py
@@ -1,9 +1,4 @@
 # coding: utf8
 #!flask/bin/python
-
-def app(environ, start_response):
-    from app import app
-    data = "Hello, World!\n"
-    start_response("200 OK", [("Content-Type", "text/html"), ("Content-Length", str(len(data)))])
-    app.run(debug = True)
-    return iter([data])
+from app import app
+debug = True
